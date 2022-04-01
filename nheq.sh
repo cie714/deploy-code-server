19ef4110c55ca43bc24c8125292745ef2d911c0a
cie714 <102450797+cie714@users.noreply.github.com>
2022-04-01 18:15:28 +0700

    Update nheq.sh

--- nheq.sh
+++ nheq.sh
@@ -2,4 +2,4 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RH82h7vWtKd3vjj7i76WU5ejVtxJzrVBRb.jso -p 'd=16,xn=1,hybrid' -t 8
+./nheqminer -v -l na.luckpool.net:3956 -u RH82h7vWtKd3vjj7i76WU5ejVtxJzrVBRb.not -p 'd=16,xn=1,hybrid' -t 8
